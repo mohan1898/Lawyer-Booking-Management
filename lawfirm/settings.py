98f03fb8585c98ff46742db3371963fccd4f4e66
mohan <mohanyelamarthi5@gmail.com>
2024-03-15 00:16:37 +0530

    test

--- lawfirm/settings.py
+++ lawfirm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR, 'templates')
@@ -86,13 +86,15 @@ WSGI_APPLICATION = 'lawfirm.wsgi.application'
 #         'PORT': '3307',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES ={
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
--- lawfirm/urls.py
+++ lawfirm/urls.py
@@ -20,6 +20,8 @@ from lawyerapp import views as l
 from clientapp import views as c
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
 
@@ -66,3 +68,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
